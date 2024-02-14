f473c7bb427bcb5d4eecad8f8dde19fa5e764291
Gabriel Mackenzie <113994361+Kenn-243@users.noreply.github.com>
2024-02-14 21:39:23 +0700

    Update Main.java

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -3,7 +3,8 @@ package Test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");	
+		System.out.println("Hello World!");
+		System.out.println("Ditamah di Repository langsung");
 	}
 
 }
